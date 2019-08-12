20b67512c3f4b96c2c44fb2b0687747c3dad243d
Andrew Savetchuk <andrew.savetchuk@gmail.com>
2019-08-12 12:16:55 +0300

    CRUD for replies, Resources for categories and replies

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class QuestionResource extends JsonResource
+class CategoryResource extends JsonResource
 {
   /**
    * Transform the resource into an array.
@@ -16,11 +16,8 @@ class QuestionResource extends JsonResource
   public function toArray($request)
   {
     return [
-      'title'      => $this->title,
-      'slug'       => $this->slug,
-      'body'       => $this->body,
-      'created_at' => $this->created_at->diffForHumans(),
-      'user'       => $this->user->name,
+      'id'   => $this->id,
+      'name' => $this->name,
     ];
   }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
 
 Route::apiResource('/questions', 'QuestionController');
+
 Route::apiResource('/categories', 'CategoryController');
+
+Route::apiResource('/questions/{question}/replies', 'ReplyController');
