689e3d12c62714220bd12df5ad5ef2925aa80e8d
Andrew Savetchuk <andrew.savetchuk@gmail.com>
2019-08-12 12:23:44 +0300

    Like & Dislike system for replies

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ Route::apiResource('/questions', 'QuestionController');
 Route::apiResource('/categories', 'CategoryController');
 
 Route::apiResource('/questions/{question}/replies', 'ReplyController');
+
+Route::post('/like/{reply}', 'LikeController@like');
+
+Route::delete('/dislike/{reply}', 'LikeController@dislike');
