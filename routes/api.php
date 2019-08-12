23afcd7607e64fedbd5f6f8aa7cfa4eab0fe32b7
Andrew Savetchuk <andrew.savetchuk@gmail.com>
2019-08-12 20:12:47 +0300

    JWT implemented

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,13 @@ Route::apiResource('/questions/{question}/replies', 'ReplyController');
 Route::post('/like/{reply}', 'LikeController@like');
 
 Route::delete('/dislike/{reply}', 'LikeController@dislike');
+
+Route::group([
+  'middleware' => 'api',
+  'prefix'     => 'auth'
+], function ($router) {
+  Route::post('login', 'AuthController@login');
+  Route::post('logout', 'AuthController@logout');
+  Route::post('refresh', 'AuthController@refresh');
+  Route::post('me', 'AuthController@me');
+});
